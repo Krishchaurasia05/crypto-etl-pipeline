6b75166361cb8111092e9dcfc24df934aa1936f9
Krish Chaurasia <chaurasiakrish10@gmail.com>
2026-07-10 15:15:15 +0530

    just commit it

--- tests/save_sample_data.py
+++ tests/save_sample_data.py
@@ -1,9 +1,12 @@
 # Extract coins data and save it as a json file for transfroming 
 import sys
-sys.path.append('C:/Users/krish/Music/crypto-etl-pipeline/src')
+import os
+path = os.path.dirname(__file__)
+full_path = os.path.join(path, 'sample_data.json')
+sys.path.append('C:/Users/krish/OneDrive/Projects/crypto-etl-pipeline/src')
 from extract import get_coin_data
 import json
 
 coins = get_coin_data()
-with open('sample_data.json', 'w') as file:
+with open(full_path, 'w') as file:
     json.dump(coins,file,indent=4)
