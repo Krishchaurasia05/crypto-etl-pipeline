1040c9745b8820bd96f8fc496c177df3c6cd13ca
Krish Chaurasia <chaurasiakrish10@gmail.com>
2026-07-10 15:30:55 +0530

    altered this file

--- tests/save_sample_data.py
+++ tests/save_sample_data.py
@@ -1,9 +1,14 @@
 # Extract coins data and save it as a json file for transfroming 
 import sys
 import os
+
+
 path = os.path.dirname(__file__)
 full_path = os.path.join(path, 'sample_data.json')
-sys.path.append('C:/Users/krish/OneDrive/Projects/crypto-etl-pipeline/src')
+project_root = os.path.dirname(path)
+src_path = os.path.join(project_root, 'src')
+sys.path.append(src_path)
+
 from extract import get_coin_data
 import json
 
