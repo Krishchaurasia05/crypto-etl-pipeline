b56dd45e73595a29acc886c5d4a2e7373a135d79
Krish Chaurasia <chaurasiakrish10@gmail.com>
2026-07-12 22:26:13 +0530

    Created main.py

--- src/main.py
+++ src/main.py
@@ -1,2 +1,9 @@
 from extract import get_coin_data
-from Transform import 
+from transform import transform_raw_data
+from load import load_data
+
+raw_data = get_coin_data()
+
+dim_coins, fact_coin_price_snapshot = transform_raw_data(raw_data= raw_data)
+
+load_data(dim_coins=dim_coins,fact_coin_price_snapshot=fact_coin_price_snapshot)
